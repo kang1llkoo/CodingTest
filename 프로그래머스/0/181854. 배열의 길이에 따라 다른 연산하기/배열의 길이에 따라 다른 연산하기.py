a19a5e7407c2c78d864018c4682fb222116d9e93
kang1llkoo <130503353+kang1llkoo@users.noreply.github.com>
2025-01-14 11:25:55 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -0,0 +1,15 @@
+def solution(arr, n):
+    answer = []
+    if len(arr) % 2 != 0:
+        for i, num in enumerate(arr):
+            if i % 2 == 0:
+                answer.append(num + n)
+            else:
+                answer.append(num)
+    else:
+        for i, num in enumerate(arr):
+            if i % 2 != 0:
+                answer.append(num + n)
+            else:
+                answer.append(num)
+    return answer
