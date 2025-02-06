a7d6129ec6f9c760aad4d67993d7c94c393d81c7
kang1llkoo <130503353+kang1llkoo@users.noreply.github.com>
2025-02-06 10:52:07 +0900

    Update 타겟 넘버.py

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,14 @@
+from collections import deque
+
 def solution(numbers, target):
-    answer = 0
-    return answer
+    return = 0
+    queue = deque([(numbers[0], 0), (-numbers[0], 0)])
+    while queue:
+        value, idx = queue.popleft()
+        if idx == len(numbers) -1:
+            if value == target:
+                result += 1
+        else:
+            idx += 1
+            queue = [(value + numbers[idx], idx), (value - numbers[idx], idx)]
+    return result
