abc6201f622c0fe7ad8815a8c889cd941cb5a274
kang1llkoo <130503353+kang1llkoo@users.noreply.github.com>
2025-03-06 16:41:39 +0900

    [Silver V] Title: 단어 정렬, Time: 568 ms, Memory: 37532 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,11 @@
+n = int(input())
+words = set()
+
+for _ in range(n):
+    word = input().strip()
+    words.add(word)
+
+sorted_words = sorted(words, key=lambda x: (len(x), x))
+
+for word in sorted_words:
+    print(word)
