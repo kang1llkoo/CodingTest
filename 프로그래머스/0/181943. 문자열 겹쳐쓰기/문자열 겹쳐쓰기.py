64f9aebef32c70575ccb8cba7d047fa8606887fd
kang1llkoo <130503353+kang1llkoo@users.noreply.github.com>
2025-06-02 15:59:24 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,9 +1,2 @@
 def solution(my_string, overwrite_string, s):
-    L1 = list(my_string)
-    L2 = list(overwrite_string)
-    j = 0
-    for i in range(s, s+len(L2)):
-        L1[i] = L2[j]
-        j += 1
-    answer = ''.join(L1)
-    return answer
+    return my_string[:s]+overwrite_string+my_string[s+len(overwrite_string):]
