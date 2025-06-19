2777e146072da32d2cf13f6886f6a6ad9e9f287b
kang1llkoo <130503353+kang1llkoo@users.noreply.github.com>
2025-06-19 10:55:38 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 37004 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,15 +1,18 @@
 from string import ascii_lowercase
 
-alpha_dict = {char: idx for idx, char in enumerate(ascii_lowercase, start = 1)}
+alpha_dict = {char: idx for idx, char in enumerate(ascii_lowercase, start=1)}
 
 n = int(input())
-str = input()
-m = 31
+input_str = input()
+r = 31
+M = 1234567891
 result = 0
+power = 1
 
 for i in range(n):
-    char = str[i]
+    char = input_str[i]
     value = alpha_dict[char]
-    result += value * (m**i)
+    result = (result + value * power) % M
+    power = (power * r) % M
 
 print(result)
